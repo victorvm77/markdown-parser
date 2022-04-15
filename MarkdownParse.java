3ab322ece4969a6a6bc9ae893e149ba99c01d203
victorvm77 <vicgut770@gmail.com>
2022-04-15 10:52:55 -0700

    Markdown Error
    Exception in thread "main" java.lang.OutOfMemoryError: Java heap space
            at java.base/java.util.Arrays.copyOfRange(Arrays.java:4031)
            at java.base/java.lang.StringLatin1.newString(StringLatin1.java:767)
            at java.base/java.lang.String.substring(String.java:1914)
            at MarkdownParse.getLinks(MarkdownParse.java:19)
            at MarkdownParse.main(MarkdownParse.java:30)
    
    [link1](https://something.com)
    [link2](some-thing.html)
    [Link3] :https://docs.google.com/document/d/1LnSfvTG_Hn2fxDtFMuhBhJqsf9336Bm1ljux2Af9FqE/edit

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -19,7 +19,7 @@ public class MarkdownParse {
             toReturn.add(markdown.substring(openParen + 1, closeParen));
             currentIndex = closeParen + 1;
         }
-
+                
         return toReturn;
     }
 
--- test-file.md
+++ test-file.md
@@ -2,3 +2,4 @@
 
 [link1](https://something.com)
 [link2](some-thing.html)
+[Link3] :https://docs.google.com/document/d/1LnSfvTG_Hn2fxDtFMuhBhJqsf9336Bm1ljux2Af9FqE/edit 
