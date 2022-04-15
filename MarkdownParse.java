81f6e9dc4104c92053ba5c61b6370f7ba1853f23
httrieu <103277743+httrieu@users.noreply.github.com>
2022-04-15 11:20:50 -0700

    Starting with link or image fixed

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -17,10 +17,17 @@ public class MarkdownParse {
             int closeBracket = markdown.indexOf("]", openBracket);
             int openParen = markdown.indexOf("(", closeBracket);
             int closeParen = markdown.indexOf(")", openParen);
-            if (markdown.charAt(openBracket-1) == '!')
+            if (markdown.charAt(0) == '!')
             {
                 currentIndex = closeParen + 1;
                 continue;
+            }
+            if (openBracket != 0) {
+                if (markdown.charAt(openBracket-1) == '!')
+                {
+                    currentIndex = closeParen + 1;
+                    continue;
+                }
             }
             toReturn.add(markdown.substring(openParen + 1, closeParen));
             currentIndex = closeParen + 1;
