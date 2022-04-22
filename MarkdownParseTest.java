bd167a48ab414c5f6c3ff9815bfe68f8a3272e6d
httrieu <103277743+httrieu@users.noreply.github.com>
2022-04-22 10:53:33 -0700

    more tests

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -21,4 +21,38 @@ public class MarkdownParseTest {
         expected.add("some-thing.html");
         assertEquals(expected, MarkdownParse.getLinks(content));
     }
+
+    @Test
+    public void getLinks2() throws IOException {
+        Path fileName = Path.of("test-file2.md");
+        String content = Files.readString(fileName);
+        ArrayList<String> expected = new ArrayList<>();
+        expected.add("https://www.microsoft.com/en-us/store/apps/windows");
+        assertEquals(expected, MarkdownParse.getLinks(content));
+    }
+
+    @Test
+    public void getLinks3() throws IOException {
+        Path fileName = Path.of("test-file3.md");
+        String content = Files.readString(fileName);
+        ArrayList<String> expected = new ArrayList<>();
+        expected.add("https://something.com");
+        assertEquals(expected, MarkdownParse.getLinks(content));
+    }
+
+    @Test
+    public void getLinks4() throws IOException {
+        Path fileName = Path.of("test-file4.md");
+        String content = Files.readString(fileName);
+        ArrayList<String> expected = new ArrayList<>();
+        boolean exceptionThrown = false;
+        try {
+            MarkdownParse.getLinks(content);
+        }
+        catch (IOException e) {
+            exceptionThrown = true;
+        }
+        assertTrue("Except thrown for NullPointerException", 
+            exceptionThrown); 
+    }
 }
