221ffe2c7f5c37df14b9fb10aa6ed09188c2526a
httrieu <103277743+httrieu@users.noreply.github.com>
2022-05-06 10:33:24 -0700

    update print

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -49,5 +49,6 @@ public class MarkdownParse {
         String content = Files.readString(fileName);
         ArrayList<String> links = getLinks(content);
 	    System.out.println(links);
+        System.out.println("HI");
     }
 }
