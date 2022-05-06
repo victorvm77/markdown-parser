3879fcdd646705bbf19f19240d6b3489928bcf4d
httrieu <103277743+httrieu@users.noreply.github.com>
2022-05-06 10:44:10 -0700

    idk

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -1,5 +1,3 @@
-<<<<<<< HEAD
-//https://howtodoinjava.com/java/io/java-read-file-to-string-examples/
 
 import java.io.IOException;
 import java.nio.file.Files;
@@ -52,59 +50,3 @@ public class MarkdownParse {
 	    System.out.println(links);
     }
 }
-=======
-//https://howtodoinjava.com/java/io/java-read-file-to-string-examples/
-
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.util.ArrayList;
-
-public class MarkdownParse {
-
-    public static ArrayList<String> getLinks(String markdown) throws IOException {
-        ArrayList<String> toReturn = new ArrayList<>();
-        // find the next [, then find the ], then find the (, then read link upto next )
-        int currentIndex = 0;
-        while(currentIndex < markdown.length()) {
-            //System.out.println("THIS IS CURRENT INDEX: " + currentIndex);
-            int openBracket = markdown.indexOf("[", currentIndex);
-            int closeBracket = markdown.indexOf("]", openBracket);
-            int openParen = markdown.indexOf("(", closeBracket);
-            int closeParen = markdown.indexOf(")", openParen);
-            if (openBracket > 0) {
-                if (markdown.charAt(openBracket-1) == '!')
-                {
-                    currentIndex = closeParen + 1;
-                    continue;
-                }
-            }
-            if (openBracket == -1) {
-                throw new IllegalArgumentException("Invalid Input!");
-            }
-            if (openParen == -1) {
-                throw new IllegalArgumentException("Invalid Input!");
-            }
-            if (closeBracket < openBracket) {
-                throw new IllegalArgumentException("Invalid Input!");
-            }
-            toReturn.add(markdown.substring(openParen + 1, closeParen));
-            currentIndex = closeParen + 1;  
-        }
-        if (markdown.length() == 0) {
-            throw new IOException("Empty File!");
-        }
-        //System.out.println("THIS IS CURRENT INDEX: " + currentIndex);
-        return toReturn;
-    }
-
-
-    public static void main(String[] args) throws IOException {
-        Path fileName = Path.of(args[0]);
-        String content = Files.readString(fileName);
-        ArrayList<String> links = getLinks(content);
-	    System.out.println(links);
-        System.out.println("HI");
-    }
-}
->>>>>>> 221ffe2c7f5c37df14b9fb10aa6ed09188c2526a
